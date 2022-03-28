4ed84bc8c8e36a5a9b5a5b6235775177348f9d54
jordanos <y.dereje@yahoo.com>
2022-03-28 15:20:16 +0300

    chors(project_setup): project setup

--- web/src/app/index.tsx
+++ web/src/app/index.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Todo</header>
+      <header className="App-header">Todo app</header>
     </div>
   );
 }
