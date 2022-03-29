0a788ff991dfe1602e1f49db943fb7de934cdab2
jordanos <y.dereje@yahoo.com>
2022-03-29 11:27:15 +0300

    chore(web): basic setup done

--- web/src/app/index.tsx
+++ web/src/app/index.tsx
@@ -1,9 +1,11 @@
+import Home from 'pages/home';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Todo app</header>
+      <Home />
     </div>
   );
 }
