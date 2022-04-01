bede3f1515960bf038a675ff57ceb47a367461f6
jordanos <y.dereje@yahoo.com>
2022-04-01 16:05:05 +0300

    fix(lane height): lane max height set

--- web/src/shared/components/Board/index.tsx
+++ web/src/shared/components/Board/index.tsx
@@ -1,47 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Board from 'react-trello';
 import CustomCard from 'shared/components/CustomCard';
 import { colors, sizes } from 'shared/utils/Styles';
 import { Header as LaneHeader } from '../CustomLane';
 
-interface Props {
-  data?: any;
+interface props {
+  controller: Function;
+  tasks?: [];
 }
 
-const CustomBoard: React.FC<Props> = ({ data }) => {
-  const components = {
-    Card: CustomCard,
-    LaneHeader: LaneHeader,
-  };
+const components = {
+  Card: CustomCard,
+  LaneHeader: LaneHeader,
+};
 
-  const laneStyle = {
-    background: 'rgba(0,0,0,0.03)',
-    width: `calc(${sizes.cardWidth} + 20px)`,
-    // height: `calc(100vh - calc(${sizes.navHeight} + 50px))`,
+const laneStyle = {
+  background: 'rgba(0,0,0,0.03)',
+  width: `calc(${sizes.cardWidth} + 20px)`,
+  maxHeight: `calc(100vh - calc(${sizes.navHeight} + 50px))`,
+  scrollbarColor: `${colors.primary} transparent`,
+};
 
-    maxHeight: '100%',
-    scrollbarColor: `${colors.primary} transparent`,
-  };
+const boardStyle = {
+  background: `${colors.backgroundMedium}`,
+  position: 'fixed',
+  top: 0,
+  bottom: 0,
+  right: 0,
+  left: 0,
+  paddingLeft: '100px',
+  paddingTop: '80px',
+};
 
-  const boardStyle = {
-    background: `${colors.backgroundMedium}`,
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    paddingLeft: '100px',
-    paddingTop: '80px',
-  };
-
+const CustomBoard: React.FC<props> = ({ controller, tasks }) => {
   return (
     <Board
       style={boardStyle}
-      data={data}
+      data={controller(tasks)}
       components={components}
       laneStyle={laneStyle}
+      editable={true}
     />
   );
 };
 
-export default CustomBoard;
+const mapStateToProps = (state: any) => {
+  return { tasks: state.task.tasks };
+};
+
+export default connect(mapStateToProps)(CustomBoard);
