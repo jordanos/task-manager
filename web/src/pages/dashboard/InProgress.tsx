5bd15e35ff4fb96aae8f59532a6894d5114c52fb
jordanos <y.dereje@yahoo.com>
2022-04-05 17:15:04 +0300

    refactor: code rafactor

--- web/src/pages/dashboard/InProgress.tsx
+++ web/src/pages/dashboard/InProgress.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Card } from 'shared/components/Board/BoardController';
 import CustomCard from 'shared/components/CustomCard';
 import Title from 'shared/components/Helpers/Title';
 import StyledWrapper from 'shared/components/Wrappers/Styles';
+import { Task } from 'shared/store/reducers/taskReducer';
+
+const task: Task = {
+  id: '1',
+  description: `Sed vitae lobortis nulla, ut vulputate augue. 
+        Nullam mollis non ante et consequat. Cras quis dapibus augue. 
+        Phasellus nec fermentum mauris. Aenean et eros ut erat gravida rhoncus a et velit.`,
+  title: 'New Task',
+  date: new Date(),
+  status: 'todo',
+  assignedTo: 'myself',
+};
+
+const card: Card = {
+  id: task.id,
+  task,
+  draggable: true,
+  onEdit: null,
+  onDelete: () => {},
+};
 
 const InProgress: React.FC = () => {
   return (
@@ -14,23 +35,13 @@ const InProgress: React.FC = () => {
         onEdit={null}
         onClick={() => {}}
         onDelete={null}
-        id="1"
-        description={`Sed vitae lobortis nulla, ut vulputate augue. 
-        Nullam mollis non ante et consequat. Cras quis dapibus augue. 
-        Phasellus nec fermentum mauris. Aenean et eros ut erat gravida rhoncus a et velit.`}
-        title="New Task"
-        date={new Date()}
+        task={task}
       />
       <CustomCard
         onEdit={null}
         onClick={() => {}}
         onDelete={null}
-        id="1"
-        description={`Sed vitae lobortis nulla, ut vulputate augue. 
-        Nullam mollis non ante et consequat. Cras quis dapibus augue. 
-        Phasellus nec fermentum mauris. Aenean et eros ut erat gravida rhoncus a et velit.`}
-        title="New Task"
-        date={new Date()}
+        task={task}
       />
     </StyledWrapper>
   );
